d40b4c250f7d1c0c6a6c198b3e1ea69e0049830e
SuperSonic <supersonictw@users.noreply.github.com>
2015-10-05 15:27:51 +0800

    Create syb.py

---
+++ syb.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    Star Yuuki Bot
+    ~~~~~~~~~~~
+    LineClient for sending and receiving message from LINE server.
+    Copyright: (c) 2015 SuperSonic Software Foundation and  Star Inc.
+    Website:
+        SuperSonic Software Foundation: http://supersonic-org.cf
+        Star Inc.: http://startw.cf
+    License: Mozilla Public License 2.0
+"""
+
+print "Come Soon..."
